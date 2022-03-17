ce484e7589805177ab3f67f0e13b031e3b6fbe65
diekof <ddonizetecorrea@gmail.com>
2022-03-16 21:52:08 -0300

    ajuste controller

--- api/controller.js
+++ api/controller.js
@@ -29,11 +29,12 @@ var controllers = {
             });
     },
     convertHtmlPdfPost: function(req,res){
-        let options = { format: 'A4', path: './pdf/convert.pdf', displayHeaderFooter: true, headerTemplate: 'teste - cabecalho', footerTemplate: 'teste - rodape' };
+        const nomeArquivo = 'pdf/convert.pdf';
+        let options = { format: 'A4', path: nomeArquivo, displayHeaderFooter: true, headerTemplate: 'teste - cabecalho', footerTemplate: 'teste - rodape' };
         let file = { content: req.body.conteudo };
         html_to_pdf.generatePdf(file, options)
             .then((re) => {
-                res.download('./pdf/convert.pdf')
+                res.download(nomeArquivo)
                 // res.status(200).send({
                 //     isSuccess: true,
                 // })
