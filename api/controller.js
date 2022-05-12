26349a9ca5e7d45a299af9ab56db0738452778dc
diekof <ddonizetecorrea@gmail.com>
2022-05-12 10:24:09 -0300

    ajuste

--- api/controller.js
+++ api/controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const html_to_pdf = require('html-pdf-node');
-
+const fs = require('fs');
 
 var properties = require('../package.json')
 
@@ -32,12 +32,16 @@ var controllers = {
         const nomeArquivo = req.body.nome_arquivo ? req.body.nome_arquivo : 'pdf/convert.pdf';
         let options = { format: 'A4', path: nomeArquivo, displayHeaderFooter: true, headerTemplate: 'teste - cabecalho', footerTemplate: 'teste - rodape' };
         let file = { content: req.body.conteudo };
+        
         html_to_pdf.generatePdf(file, options)
             .then((re) => {
-                res.status(200).download(nomeArquivo)
-                // res.status(200).send({
-                //     isSuccess: true,
-                // })
+                var bitmap = fs.readFileSync(nomeArquivo);
+
+                // Create a buffer from the string and return the results
+                var vase64= new Buffer.from(bitmap).toString('base64');
+
+                // res.status(200).download(nomeArquivo)
+                res.status(200).send({base64: vase64,filename:nomeArquivo})
             }).catch((err) => {
                 res.status(200).send({
                     isSuccess: false,
